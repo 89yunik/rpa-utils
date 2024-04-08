52a3cdf40ec6633bb9ba3c9d2c05dd81503d4a63
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2024-04-08 20:43:08 +0900

    feat : txt read, update 함수

--- src/components/utils/etc.py
+++ src/components/utils/etc.py
@@ -8,3 +8,13 @@ def close_program(program:str):
 def close_programs(programs:List[str]):
     for program in programs: close_program(program=program)
 
+def get_txt(txt_path:str)->List[str]:
+    f = open(txt_path, 'r')
+    data = f.readlines()
+    f.close()
+    return data
+
+def update_txt(txt_path:str, data:List[str]):
+    f = open(txt_path, 'w')
+    f.writelines(data)
+    f.close()
