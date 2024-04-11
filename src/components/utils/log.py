55329617d20cbf10f5f2c6e0966cbb10174b2183
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2024-04-11 17:09:48 +0900

    setting : logging

--- src/components/utils/log.py
+++ src/components/utils/log.py
@@ -1,1 +1,17 @@
-from traceback import format_exc
+from datetime import datetime
+from os import path, getcwd
+import logging
+
+file_name_datefmt:str = '%Y.%m.%d'
+today:str = datetime.today().strftime(file_name_datefmt)
+log_dir_name:str = 'log'
+log_txt_file_path:str = path.join(getcwd(), log_dir_name, f'{today}.log')
+
+logging.basicConfig(
+    filename=log_txt_file_path,
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s:%(message)s",
+    datefmt="%Y-%m-%d %I:%M:%S %p"
+)
+
+logger = logging.getLogger()
