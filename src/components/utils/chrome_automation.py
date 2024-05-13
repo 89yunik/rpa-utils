fe3bca863264592081c2805c73010f123932d3ea
[SW2/서울/1반] 윤익 <89yunik@gmail.com>
2024-05-13 21:33:24 +0900

    fix : return log 추가

--- src/components/utils/chrome_automation.py
+++ src/components/utils/chrome_automation.py
@@ -11,20 +11,36 @@ class ChromeAutomation:
         self.driver:None = None
         
     @error_handler()
-    def start_browser(self, driver_path:str): self.driver:WebDriver = webdriver.Chrome(service=Service(driver_path))
+    def start_browser(self, driver_path:str, wait_time:int=10) -> str: 
+        self.driver:WebDriver = webdriver.Chrome(service=Service(driver_path))
+        self.driver.implicitly_wait(wait_time)
+
+        start_browser_log = 'Browser started'
+        return start_browser_log
 
     @error_handler()
-    def open_url(self, url:str): self.driver.get(url)
+    def open_url(self, url:str) -> str: 
+        self.driver.get(url)
+
+        open_url_log = 'URL opened'
+        return open_url_log
 
     @error_handler()
-    def quit_browser(self):
-        if self.driver: self.driver.quit()
+    def quit_browser(self) -> str:
+        if self.driver: 
+            self.driver.quit()
+
+            quit_browser_log = 'Browser closed'
+            return quit_browser_log
 
     @error_handler()
-    def control_elements(self, xpath_values:dict):
+    def control_elements_by_xpath(self, xpath_values:dict) -> str:
         for xpath, value in xpath_values.items():
             web_element:WebElement = self.driver.find_element(By.XPATH, xpath)
             web_element_tag_name:str = web_element.tag_name.lower()
             if web_element_tag_name=='select': Select(web_element).select_by_value(value)
             elif web_element_tag_name=='input': web_element.clear(); web_element.send_keys(value)
             elif web_element_tag_name=='a' or web_element_tag_name=='button': web_element.click()
+        
+        control_elements_by_xpath_log = 'Element control finished'
+        return control_elements_by_xpath_log
--- src/components/utils/outlook_automation.py
+++ src/components/utils/outlook_automation.py
@@ -1,22 +1,29 @@
-import win32com.client as w32
-from .etc import kill_task
+from typing import Union
+import win32com.client
 from .error_handler import error_handler
 
 class OutlookAutomation:
-    app = w32.gencache.EnsureDispatch('Outlook.Application')
-
-    @staticmethod
+    def __init__(self) -> None:
+        self.outlook: win32com.client.Dispatch = win32com.client.Dispatch("Outlook.Application")
+        self.namespace: win32com.client.CDispatch = self.outlook.GetNamesapce("MAPI")
+    
     @error_handler()
-    def close(): kill_task(program='OUTLOOK.EXE')
-
-    @staticmethod
-    @error_handler()
-    def send_mail(to:str, subject:str, contents:str, attachment:str=None, cc:str=None):
-        new_mail = OutlookAutomation.app.CreateItem(0)
+    def send_email(self, to:str, subject:str, body:str, attachment:Union[str, None] = None, cc:Union[str, None] = None):
+        new_mail: win32com.client.CDispatch = self.outlook.CreateItem(0)
         new_mail.To = to
         new_mail.Subject = subject
-        new_mail.HTMLBody = contents
+        new_mail.Body = body
         if attachment: new_mail.Attachments.Add(attachment)
         if cc: new_mail.CC = cc
 
         new_mail.Send()
+
+        send_email_log = f"Email sent to: {to}"
+        return send_email_log
+    
+    @error_handler()
+    def quit_outlook(self) -> str:
+        self.outlook.Quit()
+        
+        quit_outlook_log = "Outlook closed"
+        return quit_outlook_log
